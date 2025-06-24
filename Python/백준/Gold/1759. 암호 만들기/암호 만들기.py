ce3eaaf51c171f08720386fca485c21961d75956
youshouldknwme <164901821+youshouldknwme@users.noreply.github.com>
2025-06-24 22:44:03 +0900

    [Gold V] Title: 암호 만들기, Time: 64 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ Python/백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -3,23 +3,23 @@ input = sys.stdin.readline
 
 def makelist(arr,c,l):
     passwords = []
-    vowel = set(arr).intersection(set(['a','e','i','o','u']))
-    def backtracking(password,depth,v_count):
+    vowel = set(['a','e','i','o','u'])
+    def backtracking(password,depth):
         if depth == l:
-            if v_count < 1 or l - v_count < 2:
-                return
-            passwords.append("".join(password))
+            v_count = 0
+            for a in password:
+                if a in vowel:
+                    v_count += 1
+            if v_count >= 1 and l - v_count >= 2:
+                passwords.append("".join(password))
             return
         for x in words:
             if password and x<= password[-1]:
                 continue
             password.append(x)
-            if x in vowel:
-                v_count += 1
-            backtracking(password,depth+1,v_count)
-            if password.pop() in vowel:
-                v_count -= 1
-    backtracking([],0,0)
+            backtracking(password,depth+1)
+            password.pop()
+    backtracking([],0)
     return passwords
 
 l,c = map(int,input().split())
